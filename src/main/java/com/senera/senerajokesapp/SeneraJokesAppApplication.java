bd7f537fda9ace357a61d6012edee406aefd4a76
senera <mallikarachchi96@gmail.com>
2021-02-23 19:57:16 +0530

    initial commit

---
+++ src/main/java/com/senera/senerajokesapp/SeneraJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.senera.senerajokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeneraJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeneraJokesAppApplication.class, args);
+	}
+
+}
